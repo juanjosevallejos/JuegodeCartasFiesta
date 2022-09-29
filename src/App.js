0cb96d23ec93ea322e34415461fe8bc65c1105ad
unknown <juanjosevallejos.epet20@gmail.com>
2022-09-29 08:30:33 -0300

    agregando imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import { useState } from 'react';
 import React from 'react';
+import PantallaPrincipal from '.components/PantallaPrincipal'
+import Cartas from './components/Cartas'
+import PantallaJuego from './components/PantallaJuego'
+import PantallaFinal from './components/PantallaFinal'
+import Botones from './components/Botones'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-    <>
-      <Container>
-      <header>
-
-        CAL
-      </header>
-
-      </Container>
-
-    </>
-
+    <PantallaPrincipal></PantallaPrincipal>
+    <Cartas></Cartas>
+    <PantallaJuego></PantallaJuego>
+    
     </div>
   )
 }
