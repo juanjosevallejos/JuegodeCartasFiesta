3edb1540ec3350f82261a367176b70a926b2df74
14Mat <matiasbravoepet20@gmail.com>
2022-09-29 10:12:30 -0300

    Agregando cambios en PantallaJuego

--- src/components/PantallaJuego.js
+++ src/components/PantallaJuego.js
@@ -1,6 +1,28 @@
+import Botones from "./Components/Botones.js";
+import Cartas from "./components/Cartas.js";
+
 export default function PantallaJuego(props){
-    
+
+
     return(
-        <div>PantallaJuego</div>
+        <div className="PantallaJuego">
+            <div className="PantallaJuego--score grid grid 2">
+                <div className="PantallaJuego--moves">
+                <p>Movimientos</p>
+
+                </div>
+                <div className="PantallaJuego--time text-right">
+                <p>Tiempo</p>
+                </div>
+            </div>
+            <div className="PantallaJuego--cartas grid grid-4">
+               <Cartas symbol="fas fa-horse" />
+               <Cartas symbol="fas fa-tractor" />
+
+            </div>
+            <div className="text-center">
+                <Botones label="Reiniciar juego" action=""/>
+            </div>
+            </div>
     )
 }
