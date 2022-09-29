8a42df52f67892dbc089aaaa0e7205b674d089a4
unknown <juanjosevallejos.epet20@gmail.com>
2022-09-29 10:11:35 -0300

    agregando cambios pantallaprincipal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PantallaPrincipal from './components/PantallaPrincipal';
 import Cartas from './components/Cartas';
 import PantallaJuego from './components/PantallaJuego';
@@ -7,10 +7,28 @@ import Botones from './components/Botones';
 import './App.css';
 
 function App() {
+
+  const[level,setLevel] = useState(0)
+  const[stateGame, setStateGame] = useState(0)
+  
+  const changeDifficulty = () => { 
+    setLevel( level === 2 ? 0 : level + 1)
+  }
+
+  const changeStateGame = (value) => {
+    setStateGame(value)
+  }
+
   return (
     <div className="App">
 
-    <PantallaPrincipal></PantallaPrincipal>
+    <PantallaPrincipal level={level} 
+    changeDifficulty={changeDifficulty}>
+    setStart = (changeStateGame)
+    </PantallaPrincipal>
+
+
+    
     <Cartas></Cartas>
     <PantallaJuego></PantallaJuego>
     <PantallaFinal></PantallaFinal>
--- src/components/PantallaPrincipal.js
+++ src/components/PantallaPrincipal.js
@@ -1,11 +1,20 @@
 import Botones from "./Botones";
 
 export default function PantallaPrincipal(props){
-    
+    const levelText = ['facil','mediana','dificil']
+
     return(
         <div className="PantallaPrincipal text-center">
-        <h1 className="PantallaPrincipal--title">Juego de pares</h1>
-        <Botones label="iniciar juego"></Botones>
+
+         <h1 className="PantallaPrincipal--title">Juego de pares</h1>
+
+         <div className="PantallaPrincipal--menu">
+            <p>Seleccione el nivel</p>
+            <Botones label={levelText[props.level]} action={props.changeDifficulty}></Botones>
+            <br></br>
+            <Botones label="iniciar juego" action={ () => props.setSart(1) }></Botones>
+         </div>
+         <p>made with<span className="logo-react"><i className="fab fa-react"></i></span></p>
         </div>
     )
 }
