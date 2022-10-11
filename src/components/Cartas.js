6fcfbf2e64022a259eda4302be3e6ebb5e0c7b89
unknown <juanjosevallejos.epet20@gmail.com>
2022-10-11 09:27:24 -0300

    ingresando cambios y borrando ramas

--- src/components/Cartas.js
+++ src/components/Cartas.js
@@ -1,14 +1,33 @@
+import { useState } from "react"
+
 export default function Cartas(props){
 
+    const [frente, setFrente] = useState(false)
+
+const changeFrente = () => {
+    
+    setFrente(!frente)
+}
+
     return(
-        <div className="cartas rotate" >
+        <div className={`cartas ${props.rotate ? 'rotate' : ''}`}
+            data-id={props.id}
+            data-bind={props.bind}
+            onClick={ ()=> props.actionRotate(props.id, props.fixed)}
+            >
             <div className="cartas--inner">
-                <div className="cartas--front middle">
+
+                {!frente && (<div className="cartas--front middle">
                     <i className="fas fa-question"></i>
-                </div>
-                <div className="cartas--back middle">
+                </div>)}
+
+
+                {frente && (<div className="cartas--back middle">
                     <i className={props.symbol}></i>
-                </div>
+                </div>)}
+                
+            
+                
             </div>
 
 
